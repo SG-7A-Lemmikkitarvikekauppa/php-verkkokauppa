9a240d37ad3adcf2b64e010d0857dafaeda6cadb
JuhoLeppakangas <n2leju00@students.oamk.fi>
2023-04-26 13:48:32 +0300

    tuotekortti1 lisätty

--- react-kauppa/src/Pages/Etusivu.js
+++ react-kauppa/src/Pages/Etusivu.js
@@ -10,11 +10,13 @@ import laser from './kissalaser.jpg';
 import luu from './koiraluu.jpg';
 import axios from "axios";
 import Tuotekortti from '../Components/Tuotekortti';
+import tuotekortti1 from '../Components/tuotekortti1';
 
 const URL = 'http://localhost/Verkko-kauppaphp/php-kauppa/'
 
 function Etusivu() {
   const [ruoka, setRuoka] = useState([]);
+
 
   useEffect(() => {
     console.log(URL);
@@ -29,6 +31,7 @@ function Etusivu() {
         alert(error.response === undefined ? error : error.response.data.error);
       });
   }, []);
+
   
 
     return (
@@ -48,49 +51,14 @@ function Etusivu() {
         <Tuotekortti key={tuote.id} tuotelista={tuote} />
       ))}
 
-    <div className="product-item">
-      <img src= {pallo} alt="tuotteen kuva" />
-      <h3>pallo</h3>
-      <p>Kelluva pallo koiran polskutteluleikkeihin. Pehmeää TPR-kumia.</p>
-      <button>Lisää ostoskoriin</button>
-    </div>
-    <div className="product-item">
-      <img src={shampoo} alt="tuotteen kuva" />
-      <h3>Shampoo</h3>
-      <p>Hellävarainen ja kosteuttava shampoo kissalle.</p>
-      <button>Lisää ostoskoriin</button>
-    </div>
-    <div className="product-item">
-      <img src={kissaruoka} alt="tuotteen kuva" />
-      <h3>Kissan kuivaruoka</h3>
-      <p>Erityisen maistuva täysravinto sisältää leikatun kissasi hyvinvointia helliviä aineksia, kuten hyvin sulavaa kanaa, vihanneksia, hedelmiä, lohiöljyä ja prebiootteja.</p>
-      <button>Lisää ostoskoriin</button>
-    </div>
-    
-    <div className="product-item">
-      <img src={kissapuu} alt="tuotteen kuva" />
-      <h3>Raapimapuu</h3>
-      <p>Raapimapuussa on runsas valikoima oleskelu- ja vahtimistasoja, kolme kynsiä kutsuvaa tolppaa ja mm. irrotettava tyyny.</p>
-      <button>Lisää ostoskoriin</button>
-    </div>
-    <div className="product-item">
-      <img src={valjaat} alt="tuotteen kuva" />
-      <h3>Koiran valjaat</h3>
-      <p>Kestävä treenivaljas on optimaalinen valinta aktiivisille ja voimakkaille koirille. </p>
-      <button>Lisää ostoskoriin</button>
-    </div>
-    <div className="product-item">
-      <img src={laser} alt="tuotteen kuva" />
-      <h3>Laserlelu</h3>
-      <p>Klassinen laserlelu hauskuuttaa pitkään niin kissaa kuin omistajaakin.</p>
-      <button>Lisää ostoskoriin</button>
-    </div>
-    <div className="product-item">
-      <img src={luu} alt="tuotteen kuva" />
-      <h3>Puruluu</h3>
-      <p>puruluu on valmistettu laadukkaasta, yhtenäisestä naudannahasta käsin rullaamalla ja koneella puristamalla</p>
-      <button>Lisää ostoskoriin</button>
-    </div>
+    {ruoka.map((tuote) => (
+      <tuotekortti1 key={tuote.id} tuotelista={tuote} />
+    ))}
+
+   
+
+
+
   </div>
   
         </section>
