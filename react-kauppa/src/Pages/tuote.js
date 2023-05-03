2230c8dba1e35a9d9cac575d3ba138d96a2ecc12
Valtsu7 <n1kova00@students.oamk.fi>
2023-05-03 13:04:05 +0300

    yees

--- react-kauppa/src/Pages/tuote.js
+++ react-kauppa/src/Pages/tuote.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Tuote() {
-  const [tuote, setTuote] = useState([]);
+  const [tuote, setTuote] = useState({});
   const URL = 'http://localhost/Verkko-kauppaphp/php-kauppa/';
 
   let { tuoteNro } = useParams();
@@ -22,18 +22,18 @@ export default function Tuote() {
 
   return (
     <div>
+
     {tuote ? (
       <>
         <h3>{tuote.nimi}</h3>
         <p>{tuote.hinta}</p>
-        <div key={tuote.tuotenro}>
-          <button>Lisää ostoskoriin</button>
-        </div>
+        <p>{tuote.tuotenro}</p>
+        <button>Lisää ostoskoriin</button>
+        <div key={tuote.tuotenro}></div>
       </>
     ) : (
       <p>Loading...</p>
     )}
   </div>
-  
   );
 }
